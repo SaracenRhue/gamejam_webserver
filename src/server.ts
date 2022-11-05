bc4eed6008158cfec0f759cab28d76874ebde117
Johann Dietrich <johann.dietrich@gmail.com>
2022-11-05 13:10:31 +0100

    update server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
-import { Server } from 'ws';
+import { Server, WebSocket } from 'ws';
+import { Client, Login } from './interfaces';
 
-export const clients = [];
-export const handlers: { [key: string]: Function } = {};
+export const clients: Client[] = [];
+type Handler<T> = (ws: WebSocket, payload: T) => void;
+
+const onLogin: Handler<Login> = (socket, login) => {
+	clients.push({ player: { x: 0, y: 0, angle: 0 }, socket, playerId: login.playerId });
+};
+
+export const handlers: { [key: string]: Handler<any> } = {
+	Login: onLogin,
+};
 
 const wss = new Server({ port: 8080 }, () => {
 	console.log('Server started 🚀');
@@ -13,9 +22,15 @@ wss.on('connection', (ws) => {
 		try {
 			const obj = JSON.parse(data.toString());
 			const handler = handlers[obj.name as any];
-			handler?.(obj.payload);
+			handler?.(ws, obj.payload);
 		} catch (ex) {
 			console.error(ex);
+		}
+	});
+	ws.on('close', () => {
+		const index = clients.findIndex((client) => client.socket == ws);
+		if (index > -1) {
+			clients.splice(index, 1);
 		}
 	});
 });
