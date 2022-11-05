d1ff74b5ddc4ffe733209be4bdf7fe64aecde593
Johann Dietrich <johann.dietrich@gmail.com>
2022-11-05 12:49:24 +0100

    add client interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,12 +1,24 @@
+import type { WebSocket } from 'ws';
 export interface Message<T> {
-	name: string
-	payload: T
+	name: string;
+	payload: T;
 }
 
 export interface PlayerUpdate {
-	x: number
-	y: number
-	playerId: string
-	angle: number
+	x: number;
+	y: number;
+	playerId: string;
+	angle: number;
 }
 
+export interface Player {
+	x: number;
+	y: number;
+	angle: number;
+}
+
+export interface Client {
+	uuid: string;
+	socket: WebSocket;
+	player: Player;
+}
